04bb6c5234ee2b2072d99c7dae6f4652c9f3348b
LemonLamo <94323231+LemonLamo@users.noreply.github.com>
2025-01-21 00:28:15 +0100

    modified the db and the tests

--- database.sql
+++ database.sql
@@ -2,11 +2,11 @@ insert into editor values (0,'johnsi@gmail.com','john','si',1000,'physics');
 
 insert into conference values (0,'2025-12-10','2025-12-15','OPEN_FOR_SUBMISSIONS','physics','play2die',0);
 
-insert into author values(0,'author1','author','1',2000,'flowers');
-insert into author values(1,'author2','author','2',2000,'flowers');
-insert into author values(2,'author3','author','3',2000,'flowers');
-insert into author values(3,'author4','author','4',2000,'flowers');
-insert into author values(4,'author5','author','5',2000,'flowers');
+insert into author values(0,'author1@gmail.com','joe','goldberg',2000,'litterature');
+insert into author values(1,'author2@gmail.com','peach','salinger',2001,'fashion');
+insert into author values(2,'author3@gmail.com','natalie','engler',2002,'finance');
+insert into author values(3,'author4@gmail.com','rhys','montrose',2003,'medecine');
+insert into author values(4,'author5@gmail.com','ellie','alves',2004,'politics');
 
 insert into submission values(0,'how to do stuff','path','smth smth',0);
 insert into submission values(1,'guide to be si','path','smth smth',0);
@@ -20,11 +20,11 @@ insert into submission_author values(2,1);
 insert into submission_author values(3,2);
 insert into submission_author values(4,3);
 
-insert into evaluator values(0,'eval0@gmail.com','george','washington',2000,'im');
-insert into evaluator values(1,'eval1@gmail.com','sabrina ','carpenter',2000,'working');
-insert into evaluator values(2,'eval2@gmail.com','john ','nolan',2000,'late');
-insert into evaluator values(3,'eval3@gmail.com','tim ','bradford',2000,'cause im');
-insert into evaluator values(4,'eval4@gmail.com','lopez','best',2000,'a singer');
+insert into evaluator values(0,'eval0@gmail.com','george','washington',2000,'politics');
+insert into evaluator values(1,'eval1@gmail.com','sabrina ','carpenter',2000,'music');
+insert into evaluator values(2,'eval2@gmail.com','john ','nolan',2000,'LAPD');
+insert into evaluator values(3,'eval3@gmail.com','tim ','bradford',2000,'LAPD');
+insert into evaluator values(4,'eval4@gmail.com','lopez','best',2000,'Fashion');
 
 
 insert into submission_evaluator values(0,0);
